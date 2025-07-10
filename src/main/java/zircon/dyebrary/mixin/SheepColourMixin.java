146d5ea28ca04cc47647c6f94db600d830f3b4f6
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-07-10 14:36:57 -0600

    added default project stuff
    basic set up, you know the drill

---
+++ src/main/java/zircon/dyebrary/mixin/SheepColourMixin.java
@@ -0,0 +1,91 @@
+package zircon.dyebrary.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Shearable;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.util.DyeColor;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SheepEntity.class)
+public abstract class SheepColourMixin extends AnimalEntity implements Shearable {
+	@Shadow
+	@Final
+	private static TrackedData<Byte> COLOR; //somehow need to have a second tracked byte for part2 of RGB565, would allow skipping the map altogether
+
+	//second byte of RGB565 based colour storage. Will need to extend read/writeCustomDataToNbt
+	@Final
+	private static TrackedData<Byte> COLOR2 = DataTracker.registerData(SheepColourMixin.class, TrackedDataHandlerRegistry.BYTE);
+
+	private SheepColourMixin() {
+		super(EntityType.SHEEP, null);
+		throw new AssertionError();
+	}
+
+	/**
+	 * @reason Allowing >16 unique dye colors (128)
+	 * @author ADudeCalledLeo
+	 */
+	@Overwrite
+	public DyeColor getColor() {
+		byte b = dataTracker.get(COLOR);
+		return DyeColor.byId(b & 0x7F);
+	}
+
+	/**
+	 * @reason Allowing >16 unique dye colors (128)
+	 * @author ADudeCalledLeo
+	 */
+	@Overwrite
+	public void setColor(DyeColor color) {
+		byte b = dataTracker.get(COLOR);
+		dataTracker.set(COLOR, (byte) ((b & 0x80) | color.getId() % 0x7F));
+	}
+
+	/**
+	 * @reason Allowing >16 unique dye colors (128)
+	 * @author ADudeCalledLeo
+	 */
+	@Overwrite
+	public boolean isSheared() {
+		return (dataTracker.get(COLOR) & 0x80) != 0;
+	}
+
+	/**
+	 * @reason Allowing >16 unique dye colors (128)
+	 * @author ADudeCalledLeo
+	 */
+	@Overwrite
+	public void setSheared(boolean sheared) {
+		byte b = dataTracker.get(COLOR);
+		dataTracker.set(COLOR, (byte) ((b & 0x7F) | (sheared ? 0x80 : 0)));
+	}
+
+	//this is the method for changing how sheep render, something like an inject to check a custom tag would make this work no problem
+	//TODO add a property to sheep for if they are "classidyed" and if so custom colour render
+//    @Inject(method = "getDyedColor", at = @At("HEAD"), cancellable = true)
+//    private static void onGetDyedColor(DyeColor color, CallbackInfoReturnable<float[]> cir){
+//        if (color == DyeColor.WHITE){
+//            cir.setReturnValue(new float[]{0.3137254F, 0.3725901F, 0.4705882F});
+//            cir.cancel();
+//        }
+//    }
+
+	//this is the last step before sheep color rendering
+	@Inject(method = "getRgbColor", at = @At("HEAD"), cancellable = true)
+	private static void onGetRgbColor(DyeColor color, CallbackInfoReturnable<float[]> cir){
+		if (color == DyeColor.WHITE){
+			cir.setReturnValue(new float[]{0.4737254F, 0.3725901F, 0.3105882F});
+			cir.cancel();
+		}
+	}
+}
