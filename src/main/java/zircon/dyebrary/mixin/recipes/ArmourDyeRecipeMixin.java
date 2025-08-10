4b8ba7774df9f787ebc26410a2f7fee899a904a9
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-08-09 23:34:16 -0600

    armour dyeing reimplemented
    with all the grace of a sledgehammer but at long last it works

--- src/main/java/zircon/dyebrary/mixin/recipes/ArmourDyeRecipeMixin.java
+++ src/main/java/zircon/dyebrary/mixin/recipes/ArmourDyeRecipeMixin.java
@@ -10,6 +10,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.inventory.RecipeInputInventory;
 import net.minecraft.item.DyeItem;
+import net.minecraft.item.DyeableItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.ArmorDyeRecipe;
@@ -22,6 +23,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import zircon.dyebrary.ModDyeItem;
 import zircon.dyebrary.interfaces.IDyeItem;
+import zircon.dyebrary.interfaces.ModDyeableItem;
 
 import java.util.List;
 
@@ -29,6 +31,37 @@ import java.util.List;
 public class ArmourDyeRecipeMixin {
     @Unique
     List<IDyeItem> dyeList = Lists.newArrayList();
+
+    //I am at my wits end with doing this cleanly. Maybe one day I will do this less destructively
+    @Inject(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At("HEAD"), cancellable = true)
+    private void onCraft(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir){
+        ItemStack itemStack = ItemStack.EMPTY;
+
+        for (int i = 0; i < recipeInputInventory.size(); i++) {
+            ItemStack itemStack2 = recipeInputInventory.getStack(i);
+            if (!itemStack2.isEmpty()) {
+                Item item = itemStack2.getItem();
+                if (item instanceof DyeableItem) {
+                    if (!itemStack.isEmpty()) {
+                        cir.setReturnValue(ItemStack.EMPTY);
+                        cir.cancel();
+                    }
+
+                    itemStack = itemStack2.copy();
+                } else {
+                    if (!(item instanceof IDyeItem)) {
+                        cir.setReturnValue(ItemStack.EMPTY);
+                        cir.cancel();
+                    }
+
+                    dyeList.add((IDyeItem) item);
+                }
+            }
+        }
+
+        cir.setReturnValue(!itemStack.isEmpty() && !dyeList.isEmpty() ? ModDyeableItem.modBlendAndSetColor(itemStack, dyeList) : ItemStack.EMPTY);
+        cir.cancel();
+    }
 
     @WrapOperation(method = "matches(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/world/World;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", ordinal = 1))
     private Item onDyeItemCheck(ItemStack instance, Operation<Item> original){
@@ -38,35 +71,35 @@ public class ArmourDyeRecipeMixin {
         return original.call(instance);
     }
 
-    @Inject(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", shift = At.Shift.AFTER))
-    private void addCustomDyes(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir, @Local(ordinal = 1) ItemStack itemStack2){
-        if(itemStack2.getItem() instanceof ModDyeItem){
-            dyeList.add((IDyeItem) itemStack2.getItem());   //add modded dyes to list
-        }
-    }
-
-    @Definition(id = "DyeItem", type = DyeItem.class)
-    @Definition(id = "item", local = @Local(type = Item.class))
-    @Expression("item instanceof DyeItem")
-    @ModifyExpressionValue(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
-    private boolean dontExitOnDyes(boolean original, @Local Item item){
-        return original || item instanceof IDyeItem;
-    }
-
-    @Inject(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
-    private void addVanillishDyes(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir, @Local Item item){
-        dyeList.add(((IDyeItem) item));     //add vanillish dyes
-    }
-
-    @WrapWithCondition(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
-    private boolean noMoreCastCrash(List instance, Object e){
-        return e instanceof DyeItem;
-    }
-
-    @WrapOperation(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/DyeableItem;blendAndSetColor(Lnet/minecraft/item/ItemStack;Ljava/util/List;)Lnet/minecraft/item/ItemStack;"))
-    private ItemStack sendToModBlend(ItemStack stack, List<DyeItem> colors, Operation<ItemStack> original){
-
-        return stack;
-    }
+//    @Inject(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;",
+//            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", shift = At.Shift.AFTER))
+//    private void addCustomDyes(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir, @Local(ordinal = 1) ItemStack itemStack2){
+//        if(itemStack2.getItem() instanceof ModDyeItem){
+//            dyeList.add((IDyeItem) itemStack2.getItem());   //add modded dyes to list
+//        }
+//    }
+//
+//    @Definition(id = "DyeItem", type = DyeItem.class)
+//    @Definition(id = "item", local = @Local(type = Item.class))
+//    @Expression("item instanceof DyeItem")
+//    @ModifyExpressionValue(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+//    private boolean dontExitOnDyes(boolean original, @Local Item item){
+//        return original || item instanceof IDyeItem;
+//    }
+//
+//    @Inject(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
+//    private void addVanillishDyes(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir, @Local Item item){
+//        dyeList.add(((IDyeItem) item));     //add vanillish dyes
+//    }
+//
+//    @WrapWithCondition(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
+//    private boolean noMoreCastCrash(List instance, Object e){
+//        return e instanceof DyeItem;
+//    }
+//
+//    @WrapOperation(method = "craft(Lnet/minecraft/inventory/RecipeInputInventory;Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/DyeableItem;blendAndSetColor(Lnet/minecraft/item/ItemStack;Ljava/util/List;)Lnet/minecraft/item/ItemStack;"))
+//    private ItemStack sendToModBlend(ItemStack stack, List<DyeItem> colors, Operation<ItemStack> original){
+//
+//        return stack;
+//    }
 }
