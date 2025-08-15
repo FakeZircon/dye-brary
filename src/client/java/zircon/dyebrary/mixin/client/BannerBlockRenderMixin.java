0b71fb8536a13cbc504db4c27fcc91c22bdbc3eb
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-08-15 15:36:58 -0600

    created banner render mixin
    I will need some kind of plan cause banners are fukcing annoying

---
+++ src/client/java/zircon/dyebrary/mixin/client/BannerBlockRenderMixin.java
@@ -0,0 +1,9 @@
+package zircon.dyebrary.mixin.client;
+
+import net.minecraft.client.render.block.entity.BannerBlockEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BannerBlockEntityRenderer.class)
+public class BannerBlockRenderMixin {
+
+}
