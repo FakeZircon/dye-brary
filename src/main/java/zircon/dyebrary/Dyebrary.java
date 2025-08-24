2a9a4884640fa99caa7de3a0b7b1914767014663
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-08-24 14:28:30 -0600

    Added edible bonemeal
    easter egg ification of the feature isn't done yet

--- src/main/java/zircon/dyebrary/Dyebrary.java
+++ src/main/java/zircon/dyebrary/Dyebrary.java
@@ -2,16 +2,14 @@ package zircon.dyebrary;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
--- src/main/java/zircon/dyebrary/mixin/item/BonemealMixin.java
+++ src/main/java/zircon/dyebrary/mixin/item/BonemealMixin.java
@@ -1,12 +1,24 @@
 package zircon.dyebrary.mixin.item;
 
-import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 
-@Mixin(BoneMealItem.class)
-public class BonemealMixin {
+@Mixin(Items.class)
+public abstract class BonemealMixin {
     //todo make bonemeal food easteregg for users with the word "bones" in their name
-    //FoodComponents BONEMEAL = new FoodComponent().Builder().hunger;
+    @ModifyArg(
+            method = "<clinit>",
+            slice = @Slice(
+                    from = @At(value = "CONSTANT", args = "stringValue=bone_meal")
+            ),
+            at = @At(value = "INVOKE", target = "net/minecraft/item/BoneMealItem.<init>(Lnet/minecraft/item/Item$Settings;)V", ordinal = 0)
+    )
+    private static Item.Settings addFoodComps(Item.Settings settings){
+        return settings.food(new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build());
+    }
 }
